963ac696a2a7e13fdc056f732a428fd239281e4d
mecoder110 <smurtaza111@gmail.com>
2025-07-23 12:41:28 +0530

    JdbcAuthentication

--- src/main/java/com/spring_security/demo/configuration/SecurityConfig.java
+++ src/main/java/com/spring_security/demo/configuration/SecurityConfig.java
@@ -1,26 +1,48 @@
 package com.spring_security.demo.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+
+
+        //        .authorizeHttpRequests(auth -> auth
+        //            .requestMatchers("/h2-console/**", "/public", "/auth/**").permitAll() // ⬅️ Add this line
+        //            .anyRequest().authenticated()
+        //        )
+        http.csrf(csrf->csrf.disable())
+                .headers(headers -> headers.frameOptions().disable())
+                .authorizeHttpRequests(
+                        requests -> requests
+                                .requestMatchers("api/public/**","/h2-console/**")
+                                .permitAll()
+                                .anyRequest().authenticated())
                 // http.formLogin(withDefaults());
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
                 httpBasic(withDefaults());
@@ -30,14 +52,24 @@ public class SecurityConfig {
     @Bean
     UserDetailsService userDetailsService() {
 
-        UserDetails user1 = User.withUsername("ali")
-                .password("{noop}12345")
+        UserDetails user1 = User.withUsername("user")
+                .password(passwordEncoder().encode("12345"))
                 .roles("USER")
                 .build();
         UserDetails user2 = User.withUsername("admin")
-                .password("{noop}234")
+                .password(passwordEncoder().encode("12346"))
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user1, user2);
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user1);
+        jdbcUserDetailsManager.createUser(user2);
+        return jdbcUserDetailsManager;
+        //return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/spring_security/demo/controller/MyController.java
+++ src/main/java/com/spring_security/demo/controller/MyController.java
@@ -1,14 +1,28 @@
 package com.spring_security.demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
+@RequestMapping("api/")
 public class MyController {
 
-    @GetMapping("api/security")
-    public String greet(){
-        return "Hello";
+    @GetMapping("/public")
+    public String forPublic(){
+        return "Hello WORLD";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin(){
+        return "Hello ADMIN";
+    }
+
+    @PreAuthorize(("hasRole('USER')"))
+    @GetMapping("/user")
+    public String user(){
+        return "Hello USER";
     }
 }
